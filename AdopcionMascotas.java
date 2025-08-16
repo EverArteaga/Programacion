26bbe13aa181341dfb08f9a6be41952ea0dbec4a
Ever Arteaga <togeinumaki1227@gmail.com>
2025-08-15 23:48:17 -0500

    Update AdopcionMascotas.java

--- AdopcionMascotas.java
+++ AdopcionMascotas.java
@@ -4,7 +4,7 @@ public class AdopcionMascotas {
 
     private String nombre;
     private String especie; // perro, gato, etc.
-    private int edad; 
+    private int edad;
     private boolean adoptada;
 
     // Constructor para inicializar los datos de la mascota
@@ -12,7 +12,7 @@ public class AdopcionMascotas {
         this.nombre = nombre;
         this.especie = especie;
         this.edad = edad;
-        this.adoptada = false; // La mascota inicialmente no está adoptada
+        this.adoptada = false;
     }
 
     public void mostrarInfo() {
@@ -31,13 +31,25 @@ public class AdopcionMascotas {
         }
     }
 
-    public static void main(String[] args) {
-   
-    AdopcionMascotas m1 = new AdopcionMascotas("Luna", "Perro", 3);
-    
-    m1.mostrarInfo();
-    
-    m1.adoptar();
-}
-}
+    public int edadEnHumanos() {
+        if (especie.equalsIgnoreCase("Perro")) {
+            return edad * 7;
+        } else if (especie.equalsIgnoreCase("Gato")) {
+            return edad * 5;
+        } else {
+            return edad;
+        }
+    }
 
+    public static void main(String[] args) {
+        AdopcionMascotas m1 = new AdopcionMascotas("Luna", "Perro", 3);
+
+        m1.mostrarInfo();
+        
+       
+        m1.adoptar();
+        
+   
+        System.out.println("La edad de " + m1.nombre + " en anios humanos es: " + m1.edadEnHumanos());
+    }
+}
